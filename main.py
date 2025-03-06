858850fbd571e7c50ca606baac78e088963f48fa
MuskanNadeem28 <za7589506@gmail.com>
2025-03-07 00:00:48 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+
+print("Hello, World!")
+
+
+def greet(name):
+    return f"Hello, {name}!"
+
+print(greet("Muskan"))
+
+
+name = "Muskan"
+
+
+age = 21
+
+height = 5.7
+
+is_student = True
+
+grades = [95, 83, 90, 87, 74]
+
+coordinates = (3, 5)
+
+
+person = {"name": "Muskan", "age": 21, "height": 5.7}
+
+
+unique_numbers = {1, 2, 3, 4, 5}
+
+print(name, age, height, is_student, grades, coordinates, person, unique_numbers)
